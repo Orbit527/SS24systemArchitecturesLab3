6effb2a18984890f4222a585ac7e5e09556285a7
Orbit527 <peterschneider15@gmx.net>
2024-05-29 16:56:20 +0200

    implemented correct lighting

--- src/main/java/at/fhv/sysarch/lab3/pipeline/filters/Lighting.java
+++ src/main/java/at/fhv/sysarch/lab3/pipeline/filters/Lighting.java
@@ -30,14 +30,18 @@ public class Lighting implements IFilter <DataPair, DataPair>{
 
 
             //TODO: implement further
-            double factor = 1;
+
+            double factor = lightPos.getUnitVector().dot(dp.getFace().getN1().getUnitVector().toVec3());
+
+            if (factor < 0) {
+                factor = 0;
+            }
 
             double red = Math.min(dp.getColor().getRed() * factor, 1.0);
             double green = Math.min(dp.getColor().getGreen() * factor, 1.0);
             double blue = Math.min(dp.getColor().getBlue() * factor, 1.0);
 
             Color newColor = new Color(red, green, blue, dp.getColor().getOpacity());
-
 
             DataPair newDataPair = new DataPair(dp.getFace(), newColor);
 
